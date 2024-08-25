2050f78819558b746c9143a855e8b8c5b88839f4
Juyeon Hong <beive999@naver.com>
2024-08-25 19:50:38 +0900

    Add features 1. Disable add button when the input is empty 2. Enable enter key to submit 3. Reorder the list when check the checkbox

--- ex3/src/App.js
+++ ex3/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
@@ -8,21 +7,36 @@ function TodoApp() {
   const [todos, setTodos] = useState([]);
 
   const onChangeHandler = (e) => setInputText(e.target.value);
+
   const onClickHandler = () => {
       const todosCopy = todos.concat({id: nextId, todo: inputText, isChecked: false, isPriority: false});
       setTodos(todosCopy);
       setNextId(nextId + 1);
       setInputText('');
-  }
+  };
 
   const onClickRemove = (id) => {
     const updatedTodos = todos.filter(todo => todo.id !== id)
     setTodos(updatedTodos);
-  }
+  };
 
   const onChecked = (id) => {
     // isChecked 상태 변경
     const updatedTodos = todos.map(todo => todo.id === id ? {...todo, isChecked: !todo.isChecked} : todo);
+    reorder1(updatedTodos);
+  };
+
+  function reorder1(updatedTodos) {
+    updatedTodos.sort((a, b) => {
+      if (a.isPriority !== b.isPriority) {
+        return a.isPriority ? -1 : 1;
+      } else if (a.isChecked !== b.isChecked) {
+        return a.isChecked ? 1 : -1;
+      } else {
+        return a.id - b.id;
+      }
+    });
+
     setTodos(updatedTodos);
   }
 
@@ -34,18 +48,20 @@ function TodoApp() {
   //       todo.id === id ? { ...todo, isPriority: !todo.isPriority } : todo
   //     );
       
-  //     reorder(updatedTodos);
+  //     reorder2(updatedTodos);
   //     return updatedTodos;
   //   });
   // };
   
-  // function reorder(updatedTodos) {
+  // function reorder2(updatedTodos) {
   //   const todosCopy = [...updatedTodos];
   
   //   todosCopy.sort((a, b) => {
-  //     if (a.isPriority !== b.isPriority) {
+  //     if (a.isPriority !== b.isPriority) {  // priority가 서로 다르면 true를 먼저
   //       return a.isPriority ? -1 : 1;
-  //     } else {
+  //     } else if (a.isChecked !== b.isChecked) { 
+  //       return a.isChecked ? 1 : -1;
+  //     } else {  // priority가 서로 같으면 id로 순서 정하기
   //       return a.id - b.id;
   //     }
   //   });
@@ -57,14 +73,16 @@ function TodoApp() {
   const onClickAddImportance = (id) => {
     // isPriority 상태 변경
     const updatedTodos = todos.map(todo => todo.id === id ? {...todo, isPriority: !todo.isPriority} : todo);
-    // setTodos(updatedTodos);  // XXX => set함수는 한 번만 사용 가능
-    reorder(updatedTodos);
-  }
+    // setTodos(updatedTodos);  // XXX => set함수는 한 번만 사용 가능(reorder 후)
+    reorder2(updatedTodos);
+  };
 
-  function reorder(updatedTodos) {
+  function reorder2(updatedTodos) {
     updatedTodos.sort((a, b) => {
       if (a.isPriority !== b.isPriority) {  // priority가 서로 다르면 true를 먼저
         return a.isPriority ? -1 : 1;
+      } else if (a.isChecked !== b.isChecked) { 
+        return a.isChecked ? 1 : -1;
       } else {  // priority가 서로 같으면 id로 순서 정하기
         return a.id - b.id;
       }
@@ -72,6 +90,12 @@ function TodoApp() {
 
     setTodos(updatedTodos);
   }
+
+  const enterKeyDown = (e) => {
+    if (e.key === 'Enter' && inputText.trim() !== '') {
+      onClickHandler();
+    }
+  };
 
   // 방법 1
   // const todosList = todos.map(todo => (<>
@@ -85,7 +109,7 @@ function TodoApp() {
   const todosList = todos.map(todo => (<TodoCheckbox todo={todo}/>));
 
   function TodoCheckbox({todo}) {
-    return  <div key={todo.id}>
+    return  <div key={todo.id} style={{marginBottom: 10 + 'px'}}>
               <input type='checkbox' checked={todo.isChecked} onChange={() => onChecked(todo.id)}/>
               <span style={{'textDecoration':`${todo.isChecked ? 'line-through' : 'none'}`}} onClick={() => onClickAddImportance(todo.id)}> {todo.todo}</span>
               {`${todo.isPriority ? ' ❤' : ''}`}
@@ -105,14 +129,14 @@ function TodoApp() {
 
   return (
     <div className='flex-wrapper'>
-      <header><h1>오늘의 할 일! 2024.08.21</h1></header>
+      <header><h1>오늘의 할 일!</h1></header>
       
       <div className='content'>
         <h2>ToDo-List</h2>
         <ul className='list-box'>{todosList}</ul>
         <div className='add-list-box'>
-          <input className='input-text' value={inputText} onChange={onChangeHandler}/>
-          <button className='add-btn' onClick={onClickHandler}>추가하기</button>
+          <input className='input-text' value={inputText} onChange={onChangeHandler} onKeyDown={enterKeyDown}/>
+          <button className='add-btn' onClick={onClickHandler} disabled={inputText.trim() === ''}>추가하기</button>
         </div>
       </div>
 
